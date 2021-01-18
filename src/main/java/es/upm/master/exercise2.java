fa1cd0d52dc6b1e11916a1906c705d3b578113b1
empiriker <till@ueberfries.de>
2021-01-18 10:02:57 +0100

    Exercise 2. Replaced Long with Integer to test impact on execution time.

--- src/main/java/es/upm/master/exercise2.java
+++ src/main/java/es/upm/master/exercise2.java
@@ -38,6 +38,7 @@ import java.util.Iterator;
 
 public class exercise2 {
     public static void main(String[] args) throws Exception {
+        long startTime = System.nanoTime();
         final ParameterTool params = ParameterTool.fromArgs(args);
         // set up the execution environment
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -50,45 +51,45 @@ public class exercise2 {
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
         // Process the data for right types
-        SingleOutputStreamOperator<Tuple6<Long, Long, Long, Long, Long, Long>> mapStream = text.
-                map(new MapFunction<String, Tuple6<Long, Long, Long, Long, Long, Long>>() {
-                    public Tuple6<Long, Long, Long, Long, Long, Long> map(String in) throws Exception{
+        SingleOutputStreamOperator<Tuple6<Long, Long, Long, Integer, Integer, Integer>> mapStream = text.
+                map(new MapFunction<String, Tuple6<Long, Long, Long, Integer, Integer, Integer>>() {
+                    public Tuple6<Long, Long, Long, Integer, Integer, Integer> map(String in) throws Exception{
                         String[] fieldArray = in.split(",");
-                        Tuple6<Long,Long,Long,Long,Long,Long> out = new Tuple6(
+                        Tuple6<Long,Long,Long,Integer,Integer,Integer> out = new Tuple6(
                                 Long.parseLong(fieldArray[0]), // Time
                                 Long.parseLong(fieldArray[1]), // VID
                                 Long.parseLong(fieldArray[2]), // Spd
-                                Long.parseLong(fieldArray[3]), // XWay
-                                Long.parseLong(fieldArray[5]), // Dir
-                                Long.parseLong(fieldArray[6])); // Seg
+                                Integer.parseInt(fieldArray[3]), // XWay
+                                Integer.parseInt(fieldArray[5]), // Dir
+                                Integer.parseInt(fieldArray[6])); // Seg
                         return out;
                     }
                 })
-                .filter(new FilterFunction<Tuple6<Long, Long, Long, Long, Long, Long>>(){
-                    public boolean filter(Tuple6<Long, Long, Long, Long, Long, Long> in) throws Exception {
+                .filter(new FilterFunction<Tuple6<Long, Long, Long, Integer, Integer, Integer>>(){
+                    public boolean filter(Tuple6<Long, Long, Long, Integer, Integer, Integer> in) throws Exception {
                         return (in.f5 >= Long.parseLong(params.get("startSegment")) && // select relevant segments
                                 in.f5 <= Long.parseLong(params.get("endSegment")) &&
                                 in.f4 == 0); // only eastbound vehicle
                     }
                 });
 
-        KeyedStream<Tuple6<Long, Long, Long, Long, Long, Long>, Tuple> keyedStream = mapStream
-                .assignTimestampsAndWatermarks(new AscendingTimestampExtractor<Tuple6<Long, Long, Long, Long, Long, Long>>(){
-                    public long extractAscendingTimestamp(Tuple6<Long, Long, Long, Long, Long, Long> element){
+        KeyedStream<Tuple6<Long, Long, Long, Integer, Integer, Integer>, Tuple> keyedStream = mapStream
+                .assignTimestampsAndWatermarks(new AscendingTimestampExtractor<Tuple6<Long, Long, Long, Integer, Integer, Integer>>(){
+                    public long extractAscendingTimestamp(Tuple6<Long, Long, Long, Integer, Integer, Integer> element){
                         return element.f0 * 1000;
                     }})
                 .keyBy(1,4);
 
-        SingleOutputStreamOperator<Tuple6<Long, Long, Long, Long, Long, Long>> avgSpeed = keyedStream.
+        SingleOutputStreamOperator<Tuple4<Long, Long, Long, Integer>> avgSpeed = keyedStream.
                 window(TumblingEventTimeWindows.of(Time.seconds(Long.parseLong(params.get("time"))))).
                 apply(new exercise2.calculateAvgSpeed()).
-                filter(new FilterFunction<Tuple6<Long, Long, Long, Long, Long, Long>>(){
-                    public boolean filter(Tuple6<Long, Long, Long, Long, Long, Long> in) throws Exception {
+                filter(new FilterFunction<Tuple4<Long, Long, Long, Integer>>(){
+                    public boolean filter(Tuple4<Long, Long, Long, Integer> in) throws Exception {
                         return (in.f2 > Integer.parseInt(params.get("speed")));
                     }
                 });
 
-        SingleOutputStreamOperator<Tuple4<Long, Long, Integer, String>> speedersOnXway = avgSpeed.
+        SingleOutputStreamOperator<Tuple4<Long, Integer, Integer, String>> speedersOnXway = avgSpeed.
                 windowAll(TumblingEventTimeWindows.of(Time.seconds(Long.parseLong(params.get("time"))))).
                 apply(new exercise2.SpeedersOnXway());
 
@@ -98,45 +99,46 @@ public class exercise2 {
             speedersOnXway.writeAsCsv(params.get("output")).setParallelism(1);
         }
         env.execute("exercise2");
+
+        long stopTime = System.nanoTime();
+        System.out.println((stopTime - startTime)/1000000000.);
     }
 
-    public static class calculateAvgSpeed implements WindowFunction<Tuple6<Long, Long, Long, Long, Long, Long>,
-            Tuple6<Long, Long, Long, Long, Long, Long>, Tuple, TimeWindow> {
-        public void apply(Tuple tuple, TimeWindow timeWindow, Iterable<Tuple6<Long, Long, Long, Long, Long, Long>> iterable,
-                          Collector<Tuple6<Long, Long, Long, Long, Long, Long>> collector) throws Exception {
-            Iterator<Tuple6<Long, Long, Long, Long, Long, Long>> iterator = iterable.iterator();
-            Tuple6<Long, Long, Long, Long, Long, Long> first = iterator.next();
+    public static class calculateAvgSpeed implements WindowFunction<Tuple6<Long, Long, Long, Integer, Integer, Integer>,
+            Tuple4<Long, Long, Long, Integer>, Tuple, TimeWindow> {
+        public void apply(Tuple tuple, TimeWindow timeWindow, Iterable<Tuple6<Long, Long, Long, Integer, Integer, Integer>> iterable,
+                          Collector<Tuple4<Long, Long, Long, Integer>> collector) throws Exception {
+            Iterator<Tuple6<Long, Long, Long, Integer, Integer, Integer>> iterator = iterable.iterator();
+            Tuple6<Long, Long, Long, Integer, Integer, Integer> first = iterator.next();
             Long ts = 0L;
-            Long xway = 0L;
+            Integer xway = 0;
             Long VID = 0L;
             Long spd = 0L;
-            Long Seg = 0L;
             Integer count = 1;
             if(first!=null){
                 ts = first.f0;
                 xway = first.f3;
                 VID = first.f1;
                 spd = first.f2;
-                Seg = first.f5;
             }
             while(iterator.hasNext()){
-                Tuple6<Long, Long, Long, Long, Long, Long> next = iterator.next();
+                Tuple6<Long, Long, Long, Integer, Integer, Integer> next = iterator.next();
                 spd += next.f2;
                 count++;
             }
             Long avg = spd/count;
-            collector.collect(new Tuple6<Long, Long, Long, Long, Long, Long>(ts, VID, avg, xway, count.longValue(), Seg));
+            collector.collect(new Tuple4<Long, Long, Long, Integer>(ts, VID, avg, xway));
         }
     }
 
-    public static class SpeedersOnXway implements AllWindowFunction<Tuple6<Long, Long, Long, Long, Long, Long>,
-            Tuple4<Long, Long, Integer, String>, TimeWindow> {
-        public void apply(TimeWindow timeWindow, Iterable<Tuple6<Long, Long, Long, Long, Long, Long>> iterable,
-                          Collector<Tuple4<Long, Long, Integer, String>> collector) throws Exception {
-            Iterator<Tuple6<Long, Long, Long, Long, Long, Long>> iterator = iterable.iterator();
-            Tuple6<Long, Long, Long, Long, Long, Long> first = iterator.next();
+    public static class SpeedersOnXway implements AllWindowFunction<Tuple4<Long, Long, Long, Integer>,
+            Tuple4<Long, Integer, Integer, String>, TimeWindow> {
+        public void apply(TimeWindow timeWindow, Iterable<Tuple4<Long, Long, Long, Integer>> iterable,
+                          Collector<Tuple4<Long, Integer, Integer, String>> collector) throws Exception {
+            Iterator<Tuple4<Long, Long, Long, Integer>> iterator = iterable.iterator();
+            Tuple4<Long, Long, Long, Integer> first = iterator.next();
             Long ts = 0L;
-            Long xway = 0L;
+            Integer xway = 0;
             Integer count = 1;
             String vids = "[ ";
             if(first!=null){
@@ -145,7 +147,7 @@ public class exercise2 {
                 vids += first.f1 + " ";
             }
             while(iterator.hasNext()){
-                Tuple6<Long, Long, Long, Long, Long, Long> next = iterator.next();
+                Tuple4<Long, Long, Long, Integer> next = iterator.next();
                 if(ts > next.f0){
                     ts = next.f0;
                 }
@@ -153,7 +155,7 @@ public class exercise2 {
                 count += 1;
             }
             vids += "]";
-            collector.collect(new Tuple4<Long, Long, Integer, String>(ts, xway, count, vids));
+            collector.collect(new Tuple4<Long, Integer, Integer, String>(ts, xway, count, vids));
         }
     }
 
